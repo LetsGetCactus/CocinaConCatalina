3a79084275082a2fd85c1d6d0f7a54a1739982fd
Cactus <coterosieiro@gmail.com>
2025-12-05 17:57:47 +0100

    License added

--- app/src/main/java/com/letsgetcactus/cocinaconcatalina/MainActivity.kt
+++ app/src/main/java/com/letsgetcactus/cocinaconcatalina/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.letsgetcactus.cocinaconcatalina
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -22,6 +20,24 @@ import com.letsgetcactus.cocinaconcatalina.viewmodel.RecipeViewModel
 import com.letsgetcactus.cocinaconcatalina.viewmodel.RecipeViewModelFactory
 import com.letsgetcactus.cocinaconcatalina.viewmodel.UserViewModel
 import com.letsgetcactus.cocinaconcatalina.viewmodel.UserViewModelFactory
+
+
+/*
+ * Copyright (C) 2025 Catarina Otero Sieiro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ *
+ */
 
 class MainActivity : ComponentActivity() {
 
